0483a831df9620339e5adbb80c33460cfa263a09
yuval-itzhak <yuval.itzhak3@gmail.com>
2025-04-06 20:14:59 +0300

    fixing the routes error - return a promis

--- backend/src/routes/codeBlockRoutes.ts
+++ backend/src/routes/codeBlockRoutes.ts
@@ -4,7 +4,7 @@ import CodeBlock from '../models/CodeBlock';
 const router = express.Router();
 
 //get all code blocks
-router.get('/codeBlocks', async (req: Request, res: Response) => {
+router.get('/codeBlocks', async (req: Request, res: Response): Promise<any> => {
   try {
     const codeBlocks = await CodeBlock.find();
     res.json(codeBlocks);
@@ -13,17 +13,17 @@ router.get('/codeBlocks', async (req: Request, res: Response) => {
   }
 });
 
-// //get a specific code block by ID
-// router.get('/codeBlocks/:id', async (req: Request, res: Response) => {
-//   try {
-//     const codeBlock = await CodeBlock.findById(req.params.id);
-//     if (!codeBlock) {
-//       return res.status(404).send('Code block not found');
-//     }
-//     res.json(codeBlock);
-//   } catch (err) {
-//     res.status(500).send('Server error');
-//   }
-// });
+//get a specific code block by ID
+router.get('/codeBlocks/:id', async (req: Request, res: Response): Promise<any> => {
+  try {
+    const codeBlock = await CodeBlock.findById(req.params.id);
+    if (!codeBlock) {
+      return res.status(404).send('Code block not found');
+    }
+    res.json(codeBlock);
+  } catch (err) {
+    res.status(500).send('Server error');
+  }
+});
 
 export default router;
